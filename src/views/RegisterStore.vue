179aae750c56cc71c65b40b431f5eb8a0adf4a59
최민서 <133588297+cmsxi@users.noreply.github.com>
2025-07-06 23:59:36 +0900

    Feat

--- src/views/RegisterStore.vue
+++ src/views/RegisterStore.vue
@@ -659,7 +659,7 @@ const loadLocations = async () => {
         error.value = ''
         
         // 모든 지점 데이터를 한 번에 가져오기
-        const response = await jinjungsungService.getLocations()
+        const response = await jinjungsungService.getPublicLocations()
         allLocations.value = response.locations || response || []
         
         // 체크박스 선택 초기화
